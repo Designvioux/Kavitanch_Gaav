a5e670f8e6378bf40a2e17a663fc2e08aa6ecf47
Keyur Sanglikar <sanglikarkeyur@gmail.com>
2025-05-27 15:06:41 +0530

    new keyur 27/5/25

---
+++ .history/src/Components/Home_20250520170350.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import '../App.css';
+import '../h'
+import Category from './Category.jsx';
+import Hero from './Hero-Section.jsx';
+
+import Prastavit from './Prastavit.jsx';
+import Object from './Object.jsx';
+import MajaGav from './MajaGav.jsx';
+import Pratisad from './Pratisad.jsx';
+import Purva from './Purva.jsx';
+import Que from './Answer.jsx';
+import ContactPop from './PageComponents/ContactUs/KaviSamelanContactPop.jsx';
+import SuccessPopup from './PageComponents/ContactUs/SuccesPopUp.jsx';
+
+const Home = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+
+  const handleOpenPopup = (event) => {
+    setSelectedEvent(event);
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedEvent(null);
+    setShowPopup(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Hero />
+      <Prastavit onOpenPopup={handleOpenPopup} />
+
+      {showPopup && (
+        <ContactPop
+          onClose={handleClosePopup}
+          setShowSuccessPopup={setShowSuccessPopup}
+          selectedEvent={selectedEvent}
+        />
+      )}
+
+      <Category />
+      <Object />
+      <MajaGav />
+      <Pratisad />
+      <Purva />
+      <Que />
+
+      {showSuccessPopup && (
+        <SuccessPopup onClose={() => setShowSuccessPopup(false)} />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Home;
---
+++ .history/src/Components/Navbar_20250518203530.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './CSS/Navbar.css';
+import logo from "../Images/Kavitancha-logo.png";
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleHamburger = () => setIsOpen(!isOpen);
+
+  const handleLinkClick = () => setIsOpen(false);
+
+  return (
+    <div className='Navbar'>
+      <img src={logo} alt="Logo" className='logo-jakatwadi' />
+
+      <div className='Navbar-heading'>
+        <h2>माझं कवितांचं गांव</h2>
+        <h4>जकातवाडी राजधानी सातारा.</h4>
+      </div>
+
+      <div className="hamburger-icon" onClick={toggleHamburger}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+     
+    </div>
+    
+  );
+};
+
+export default Navbar;
