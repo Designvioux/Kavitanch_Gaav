35cf149afabf3f5a7be7f1acbe212d5de6eed9e4
keyursanglikar <sanglikarkeyur@gmail.com>
2025-05-31 15:09:51 +0530

    keyurs 3.09 31/5/25

---
+++ .history/src/Components/tempCodeRunnerFile_20250530173547.jsx
@@ -0,0 +1,14 @@
+mg src={magazine} alt="मासिक" />
+        </div>
+        <div className="aamcha-card" id="Masik">
+          <h3 className="ebook-titlee">मासिक</h3>
+          <p className="ebook-desc">
+          प्रत्येक महिन्यात प्रकाशित होणारं आमचं खास साहित्यिक मासिक – नवोदित आणि प्रस्थापित कवींना एकत्र आणणारं हृदयस्पर्शी व्यासपीठ. विविध कविता, लेख, संवाद आणि प्रेरणादायी साहित्य यांचा संगम – केवळ "कवितांचं गाव" मासिकात!</p>
+          <div className="ebook-buttons">
+        <Link  className="read-button" to="/Masik">वाचा</Link>
+            <button className="download-button">Download</button>
+          </div>
+        </div>
+      </div>
+    </ul>
+    </div>
